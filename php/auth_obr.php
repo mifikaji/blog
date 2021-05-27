e2f7825b90deda767a03d94b0be21b4de90ec002
mifikaji <82213199+mifikaji@users.noreply.github.com>
2021-05-27 19:39:31 +0300

    Create auth_obr.php

---
+++ php/auth_obr.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$mysqli=mysqli_connect("localhost", "a0528181_sdo_407", "14032000", "a0528181_sdo_407");
+
+if ($mysqli == false){
+    print("Error!" . mysqli_connect_error());
+}
+else{
+    $email= $_POST['email'];
+    $pass= $_POST['pass'];
+
+    $result= $mysqli->query("SELECT * FROM `users` WHERE `email`='$email' AND `pass`='$pass'");
+    if ($result->num_rows!=0){
+        print("exist");
+    }else{
+        print("don't exist");
+    };
+}
